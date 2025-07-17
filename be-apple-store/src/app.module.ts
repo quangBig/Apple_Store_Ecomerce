d98bb3e86349b942054c99fd916e3958ce24852d
quangBig <ledai16102004@gmail.com>
2025-07-18 00:51:18 +0700

    feat: Connect Database MongoDB

--- be-apple-store/src/app.module.ts
+++ be-apple-store/src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(), // load .env
+    MongooseModule.forRoot(process.env.MONGO_URI!),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
