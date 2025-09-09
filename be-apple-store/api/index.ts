326518dba46955c720a53184dab4721b21424715
quangBig <ledai16102004@gmail.com>
2025-09-09 17:29:56 +0700

    index.ts be

--- be-apple-store/api/index.ts
+++ be-apple-store/api/index.ts
@@ -1,25 +1,23 @@
-import serverlessExpress from '@vendia/serverless-express';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '../src/app.module';
+import serverlessExpress from '@vendia/serverless-express';
 import { ExpressAdapter } from '@nestjs/platform-express';
-import * as express from 'express';
+import express from 'express';
 
 let cachedServer: any;
 
 async function bootstrapServer() {
-    const expressApp = express();
-    const adapter = new ExpressAdapter(expressApp);
-
-    const app = await NestFactory.create(AppModule, adapter);
-    app.enableCors();
-    await app.init();
-
-    return serverlessExpress({ app: expressApp });
+    if (!cachedServer) {
+        const expressApp = express();
+        const app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
+        app.enableCors();
+        await app.init();
+        cachedServer = serverlessExpress({ app: expressApp });
+    }
+    return cachedServer;
 }
 
-export default async function handler(event: any, context: any) {
-    if (!cachedServer) {
-        cachedServer = await bootstrapServer();
-    }
-    return cachedServer(event, context);
+export default async function handler(req: any, res: any) {
+    const server = await bootstrapServer();
+    return server(req, res);
 }
