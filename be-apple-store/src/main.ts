92c0fe6878d782c99d86f828832b0c0e024731a7
quangBig <ledai16102004@gmail.com>
2025-09-09 21:11:30 +0700

    fix: vercel

--- be-apple-store/src/main.ts
+++ be-apple-store/src/main.ts
@@ -1,25 +1,36 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { json, urlencoded } from 'express';
-import * as express from 'express'; // Thêm import cho express
-import { join } from 'path'; // Thêm import cho join từ path
+import * as express from 'express';
+import { join } from 'path';
+import serverless from 'serverless-http';
+import { ExpressAdapter } from '@nestjs/platform-express';
+
+const server = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(server) // NestJS dùng Express của serverless
+  );
 
-  // Tăng giới hạn kích thước body lên 50MB
+  // Body limit
   app.use(json({ limit: '50mb' }));
   app.use(urlencoded({ extended: true, limit: '50mb' }));
+
+  // CORS
   app.enableCors({
-    origin: 'http://localhost:5173', // hoặc true nếu muốn cho phép tất cả
+    origin: 'http://localhost:5173',
     credentials: true,
   });
 
-
-  // Phục vụ file tĩnh từ thư mục uploads
+  // Static uploads
   app.use('/uploads', express.static(join(__dirname, '..', 'uploads')));
 
-
-  await app.listen(process.env.PORT ?? 5000);
+  await app.init(); // Không listen trực tiếp
 }
+
 bootstrap();
+
+// Export handler cho Vercel
+export const handler = serverless(server);
